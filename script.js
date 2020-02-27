5e55cc38a0d033c9170b346a1603fca0758e8290
Andy Hunt <andy.l.hunt85@gmail.com>
2020-02-27 08:51:42 -0500

    Add hover effect to images, linked to Roll20

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function nextWeek() {
+    let today = new Date();
+    if (today.getDay() == 3) {
+        let nextSession = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+        console.log(nextSession)
+    }
+}
+
 function changeTab(tabName) {
     let tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
@@ -5,5 +13,21 @@ function changeTab(tabName) {
     }
     document.getElementById(tabName).style.display = 'block';
 }
+
+window.addEventListener('load', function() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yy = today.getFullYear();
+    if (today.getDay() != 2) {
+        let nextWeek = new Date(yy, mm, dd + 9);
+        let nextdd = String(nextWeek.getDate()).padStart(2, '0');
+        let nextmm = String(nextWeek.getMonth()).padStart(2, '0');
+        let nextyy = String(nextWeek.getFullYear());
+        document.getElementById('conflux_date').innerHTML = nextmm + "/" + nextdd + "/" + nextyy;
+    } else {
+        document.getElementById('conflux_date').innerHTML = mm + "/" + dd + "/" + yy;
+    }
+})
 
 changeTab("gos_tab");
